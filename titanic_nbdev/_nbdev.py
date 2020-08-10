b3fc95af5dcfe8ae29f36d2b45bc7bf1b5d8bdfa
Michael Booth <mjboothaus@michaels-air.home>
2020-08-10 16:17:40 +1000

    progress commit of app

--- titanic_nbdev/_nbdev.py
+++ titanic_nbdev/_nbdev.py
@@ -2,9 +2,17 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"my_core_helper_function": "00_core.ipynb"}
+index = {"my_core_helper_function": "00_core.ipynb",
+         "TITANIC_DATA_FILE": "88_app.ipynb",
+         "AUTHOR_INFO": "88_app.ipynb",
+         "APP_NAME": "88_app.ipynb",
+         "SideBar": "88_app.ipynb",
+         "create_sidebar": "88_app.ipynb",
+         "create_mainscreen": "88_app.ipynb",
+         "sb": "88_app.ipynb"}
 
-modules = ["core.py"]
+modules = ["core.py",
+           "app.py"]
 
 doc_url = "https://mjboothaus.github.io/titanic_nbdev/"
 
